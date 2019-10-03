cad58a049036b7bc84e89620e56bcdd1da5fe0f6
nanamick <56085550+nanamick@users.noreply.github.com>
2019-10-03 19:34:34 +0300

    словари
    все действия сос словарями

---
+++ favorite_languages = {.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+	'Alex':'python',
+	'Ivan':'c',
+	'Max':'ruby',
+	'Olya':'python'
+}
+print('Оля сейчас учит '
+	+ favorite_languages['Olya'])
---
+++ simple_dictionary = {}.py
@@ -0,0 +1,11 @@
+simple_dictionary = {}
+alien_0 = {
+'color':'green',
+'points':5
+}
+alien_0['color'] = 'yellow'
+alien_0['speed'] = 'slow'
+del alien_0['speed']
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
